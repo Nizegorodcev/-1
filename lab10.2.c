7acf07729f47d6a8a9359fd57f91d30f57a47959
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-10-19 19:42:44 +0300

    Add files via upload

---
+++ lab10.2.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#define M_PI  3.14159265358979323846
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int factorial(int l)
+{
+	int i;
+	int d=1;
+	for (i = 1; i <= l; i++)
+	{
+		d = d * i;
+	}
+	return d;
+}
+void main(void)
+{
+	setlocale(LC_ALL, "rus");
+	int n = 0;
+	int x;
+	long float res;
+	puts("¬ведите x");
+	scanf("%i", &x);
+	float a;
+	for (res=0; n >= 0; n++)
+	{
+		a = (pow(-1, n) * pow(x, 2 * n)) / (factorial(2 * n));
+		res = res + a;
+		if (a==abs(x))
+		{
+			break;
+		}
+	}
+	printf("\n%lf", res);
+}
