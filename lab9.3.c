e1200b6bc18051639f6e311ab8b3260f29d2558c
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-11-02 19:13:26 +0300

    Add files via upload

---
+++ lab9.3.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	int a, h, x, i, j;
+	setlocale(LC_ALL, "RUS");
+	printf("¬ведите угол: ");
+	scanf("%d", &x);
+	printf("¬ведите высоту: ");
+	scanf("%d", &h);
+	printf("¬ведите сторону: ");
+	scanf("%d", &a);
+
+	if (x == 90) {
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < a; j++)
+			{
+				printf("#");
+			}
+			printf("\n");
+		}
+	}
+	else if (x > 90) {
+		int l = h - 1;
+
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < (a + h); j++) {
+
+				if (j > l && j < (a + h - i)) {
+					printf("#");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			l--;
+			printf("\n");
+		}
+	}
+	else if (x < 90) {
+		int l = a;
+
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < (a + h); j++) {
+
+				if (j < l && j >= i) {
+					printf("#");
+				}
+				else {
+					printf(" ");
+				}
+			}
+			l++;
+			printf("\n");
+		}
+	}
+	return 0;
+}
