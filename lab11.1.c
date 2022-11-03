f36d1f7e1fbec8eeddfdc953486dd80e2b464ccc
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-11-03 21:50:13 +0300

    Update lab11.1.c

--- lab11.1.c
+++ lab11.1.c
@@ -1,17 +1,30 @@
+//Вариант 4
 #define _CRT_SECURE_NO_DEPRECATE
+#define N 10
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+
 void main()
 {
-	float A[10];
+	setlocale(LC_ALL, "rus");
+	float A[N];
 	float temp;
-	for (int i = 0; i < 10; i++)
+	float s=0;
+	for (int i = 0; i < N; i++)
 	{
-		printf("a[%d]=", i+1);
+		printf("a[%d]=", i + 1);
 		scanf("%f", &temp);
-		A[i] = -temp;
-		printf("%.2f\n", A[i]);
+		A[i] = temp;
+		s += temp;
 	}
+	printf("_______________________________________\n");
+	printf("|Индекс|Старое значение|Новое значение|\n");
+	for (int i = 0; i < N; i++)
+	{
+		printf("_______________________________________\n");
+		printf("|  %i   |  %f   |   %f  |\n",i,A[i],-A[i]);
+	}
+	printf("\n\nСреднее арифметическое членов массива равно %f", s / N);
 	
 }
