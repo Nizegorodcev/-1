a10b2564b91abd7d0ae8c5089f91265486309c81
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-11-29 20:37:40 +0300

    Update kursovoi.c

--- kursovoi.c
+++ kursovoi.c
@@ -4,20 +4,53 @@
 #include <string.h>
 #include<locale.h>
 #include<time.h>
+#include<stdlib.h>
 typedef struct stroka
 {
-	char name[20], surname[20],reason[100];
-	struct tm* mytime;
-	
+	char name[20], surname[20], reason[100];
+	int day, mon, year, hour, min;
+
 }ITEM;
 ITEM A[1000];
-int n;
+int n=0;
+void Print(void);//Печать таблицы
+void includes(void);//Добавление строки
+void del(void);//Удаление строки
+void load(void);//Загрузка данных из файла
+void search(void);//Поиск элемента
+void main()
+{
+	system("chcp 1251 ");
+	setlocale(LC_ALL, "rus");
+	int v = 100;
+	while (v != 0)
+	{
+		printf("Выберите действие:\n1)Добавление новой строки.\n2)Поиск по заданным критериям.\n3)Печать массива.\n0)Выйти из программы.\n");
+		scanf("%i", &v);
+		switch (v)
+		{
+		case 1:
+			includes();
+			break;
+		case  2:
+			search();
+			break;
+		case 3:
+			Print();
+			break;
+		
+		case 0:
+			v = 0;
+			break;
+		}
+	}
+}
 void Print(void)//Печать таблицы
 {
 	for (int i = 0; i < n; i++)
 	{
-		printf("Номер строки Фамилия Имя Дата время причина");
-		printf("%i %s %s %i.%i.%i %i:%i\ %s", i + 1, A[i].surname,A[i].name,A[i].mytime->tm_mday, A[i].mytime->tm_mon, A[i].mytime->tm_year, A[i].mytime->tm_hour, A[i].mytime->tm_min, A[i].reason);
+		printf("|Номер строки|Фамилия            |Имя                 | Дата   | |время|причина\n");
+		printf("|%i          |%-20s|%-20s|%i.%i.%i| |%i:%i|%-20s|\n", i + 1, A[i].surname, A[i].name, A[i].day, A[i].mon, A[i].year, A[i].hour, A[i].min, A[i].reason);
 	}
 }
 void includes(void)
@@ -28,21 +61,21 @@ void includes(void)
 		return 0;
 	}
 	printf("Введите фамилию:");
-	scanf("%s",A[n].surname);
+	scanf("%s", &A[n].surname);
 	printf("Введите имя:");
-	scanf("%s", A[n].name);
+	scanf("%s", &A[n].name);
 	printf("Введите День:");
-	scanf("%i*", A[n].mytime->tm_mday);
+	scanf("%i*", &A[n].day);
 	printf("Введите месяц:");
-	scanf("%i*", A[n].mytime->tm_mon);
+	scanf("%i*", &A[n].mon);
 	printf("Введите год:");
-	scanf("%i", A[n].mytime->tm_year);
+	scanf("%i", &A[n].year);
 	printf("Введите часы:");
-	scanf("%i", A[n].mytime->tm_hour);
+	scanf("%i", &A[n].hour);
 	printf("Введите минуты:");
-	scanf("%i", A[n].mytime->tm_min);
+	scanf("%i", &A[n].min);
 	printf("Введите причину:");
-	scanf("%s", A[n].reason);
+	scanf("%s", &A[n].reason);
 	n++;
 }
 void del(void)
@@ -53,7 +86,7 @@ void del(void)
 	scanf("%i", &i);
 	if (i < 1 || i > n)
 	{
-		fprintf(stderr, "строка с номером %i не существует\n", i);
+		printf("строка с номером %i не существует\n", i);
 		return;
 	}
 	for (i--; i < n - 1; i++)
@@ -67,30 +100,38 @@ void load(void)
 }
 void search(void)
 {
+	ITEM name, surname;
+	ITEM hour, min;
+	ITEM* a, * b, * c, * d;
+
 	printf("Поиск по заданным критериям\n");
 	printf("Введите фамилию:");
-	scanf("%s\n", A[n].surname);
+	scanf("%s", &surname);
 	printf("Введите имя:");
-	scanf("%s\n", A[n].name);
+	scanf("%s", &name);
 	printf("Введите часы:");
-	scanf("%i\n", A[n].mytime->tm_hour);
+	scanf("%i", &hour);
 	printf("Введите минуты:");
-	scanf("%i\n", A[n].mytime->tm_min);
-}
-void main()
-{
-	setlocale(LC_ALL, "rus");
-	int v=100;
-	while (v != 0)
+	scanf("%i", &min);
+	a = &name;
+	b = &surname;
+	c = &hour;
+	d = &min;
+	ITEM* x;
+	ITEM* y;
+	ITEM* z;
+	ITEM* e;
+	for (int i = 0; i < n; i += 7)
 	{
-		printf("Выберите действие:\n1)Добавление новой строки.\n2)Поиск по заданным критериям.\n3)Печать массива.\n0)Выйти из программы.");
-		scanf("%i", &v);
-		switch (v)
+
+
+		e = &A[i];
+		x = &A[i + 1];
+		y = &A[i + 5];
+		z = &A[i + 6];
+		if (memcmp(b, e, 1000) == 0 && memcmp(a, x, 1000) == 0 && memcmp(y, c, 1000) == 0 && memcmp(z, d, 1000) == 0)
 		{
-		case 1:
-			includes();
-			break;
+			printf("|%i|%-20s|%-20s|%i.%i.%i|%i:%i|%-20s|\n", i + 1, A[i].surname, A[i].name, A[i].day, A[i].mon, A[i].year, A[i].hour, A[i].min, A[i].reason);
 		}
 	}
 }
-
