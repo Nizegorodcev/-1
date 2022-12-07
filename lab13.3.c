2433faf55305867e354abe293bc1f607741c599d
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-12-07 19:39:35 +0300

    Update lab13.3.c

--- lab13.3.c
+++ lab13.3.c
@@ -1,37 +1,38 @@
-#define _CRT_SECURE_NO_DEPRECATE
-#include<locale.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
 
-#define N 30
-void main(void)
+int main()
 {
-	char str[N];
-	system("chcp 1251 ");
-    setlocale(LC_ALL, "rus");
+    
     char s[100];
     char s1[100];
-        printf("¬‚Â‰ËÚÂ ÚÂÍÒÚ:\n");
-        fgets(s, 100, stdin);
-        int cn = 0, i = 0, j = 0, max = 0;
-        while (i != strlen(s)) {
-            if (s[i] == ' ') {
-                cn = 0;
-            }
-            else {
-                cn++;
-            }
-            if (cn > max) {
-                max = cn;
-                j = i;
-            }
-            i++;
+    system("chcp 1251 ");
+    setlocale(LC_ALL, "rus");
+    printf("–í–≤–µ–¥–∏—Ç–µ —Ç–µ–∫—Å—Ç:\n");
+    fgets(s, 100, stdin);
+    int cn=0,i=0,j=0,max=0;
+    while (i!=strlen(s)) {
+        if (s[i]==' '){
+            cn=0;
+        }else{
+            cn++;
         }
-        int k = j;
-        for (j = 0; j < max; j++) {
-            s1[j] = s[k - max + 1 + j];
+        if (cn>max){
+            max=cn;
+            j=i;
         }
-        printf("%s\n", s1);
+        i++;
+    }
+    int k = j;
+    for (j=0; j<max; j++) {
+        s1[j]=s[k-max+1+j];
+        s1[j + 1] = '\n';
+    }
+    
+    printf("%s\n",s1);
 }
