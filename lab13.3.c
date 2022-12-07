4ca2f91b7ed4f57975da7d3e06b09e6a31bd5e2e
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-12-07 19:52:12 +0300

    Update lab13.3.c

--- lab13.3.c
+++ lab13.3.c
@@ -8,31 +8,31 @@
 
 int main()
 {
-    
-    char s[100];
-    char s1[100];
-    system("chcp 1251 ");
-    setlocale(LC_ALL, "rus");
-    printf("Введите текст:\n");
-    fgets(s, 100, stdin);
-    int cn=0,i=0,j=0,max=0;
-    while (i!=strlen(s)) {
-        if (s[i]==' '){
-            cn=0;
-        }else{
-            cn++;
-        }
-        if (cn>max){
-            max=cn;
-            j=i;
-        }
-        i++;
-    }
-    int k = j;
-    for (j=0; j<max; j++) {
-        s1[j]=s[k-max+1+j];
-        s1[j + 1] = '\n';
-    }
-    
-    printf("%s\n",s1);
+
+	char s[100];
+	char s1[100];
+	system("chcp 1251 ");
+	setlocale(LC_ALL, "rus");
+	printf("Введите текст:\n");
+	fgets(s, 100, stdin);
+	int cn = 0, i = 0, j = 0, max = 0;
+	while (i != strlen(s)) {
+		if (s[i] == ' ') {
+			cn = 0;
+		}
+		else {
+			cn++;
+		}
+		if (cn > max) {
+			max = cn;
+			j = i;
+		}
+		i++;
+	}
+	int k = j;
+	for (j = 0; j < max; j++) {
+		s1[j] = s[k - max + 1 + j];
+	}
+	s1[j + 2] = '\0';
+	printf("%s\n", s1);
 }
