81f5799f16b655a3525d576d0fe19faed7d7b0ea
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2022-12-08 17:56:19 +0300

    Update lab17.1.c

--- lab17.1.c
+++ lab17.1.c
@@ -1,74 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-void sort_buble(int*ptrarr, int n);
+#include<locale.h>
+#define n 20
+void sort_buble(int* ptrarr);
 //void sort_shaker(int*ptrarr, int n);
-//void sort_select(int*ptrarr, int n);
-void sort_insert(int*ptrarr, int n);
-void Creat(int*ptrarr, int n);
+void sort_select(int*ptrarr);
+void sort_insert(int* ptrarr);
+void put(int* ptrarr);
 
 int main() {
-    int stime;
-    long ltime;
-    ltime = time(NULL);
-    stime = (unsigned) ltime/2;
-    srand(stime);
-    
-    int n;
-    printf("Введите размер массива: ");
-    scanf("%d", &n);
+    setlocale(LC_ALL, "RUS");
     int ptrarr[n];
-    Creat(ptrarr, n);
-    for (int i=0;i<n;i++){
-        printf("%i ",ptrarr[i]);
+    put(ptrarr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%i\n", ptrarr[i]);
     }
     printf("\nСортировка пузырьком\n");
-    
+
     sort_buble(ptrarr, n);
-    for (int i=0;i<n;i++){
-        printf("%i ",ptrarr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%i\n", ptrarr[i]);
     }
     printf("\nСортировка вставками\n");
-    
-    sort_insert(ptrarr, n);
-    for (int i=0;i<n;i++){
-        printf("%i ",ptrarr[i]);
+
+    sort_insert(ptrarr);
+    for (int i = 0; i < n; i++) {
+        printf("%i\n", ptrarr[i]);
     }
     printf("\n");
-
-}
-
-void Creat(int*ptrarr, int n){
-    for(int i = 0 ; i < n; i++) {
-        ptrarr[i]=(rand() % 9) + 1;
+    printf("\nСортировка простым выбором\n");
+    sort_select(ptrarr);
+    for (int i = 0; i < n; i++) {
+        printf("%i\n", ptrarr[i]);
     }
 }
 
-void sort_insert(int*a, int n){
+void put(int* ptrarr) {
+    for (int i = 0; i < n; i++) {
+        ptrarr[i] = rand();
+    }
+}
+
+void sort_insert(int* a) {
     int temp;
     int j;
-    for (int i = 1; i<n-1;i++){
-        temp=a[i];
-        j=i;
-        if (a[j-1]>temp && j>0){
-            a[j]=a[j-1];
+    for (int i = 1; i < n - 1; i++) {
+        temp = a[i];
+        j = i;
+        if (a[j - 1] > temp && j > 0) {
+            a[j] = a[j - 1];
             j--;
-        }else{
-            a[j]=temp;
+        }
+        else {
+            a[j] = temp;
         }
     }
 }
-    
-void sort_buble(int*a, int n){
+
+void sort_buble(int* a) {
     int i, j;
-    for(i = 0 ; i < n - 1; i++) {
-        for(j = 0 ; j < n - i - 1 ; j++) {
-            if(a[j] > a[j+1]) {
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
                 int tmp = a[j];
-                a[j] = a[j+1] ;
-                a[j+1] = tmp;
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
             }
+        }
+    }
+}
+void sort_select(int* ptrarr)
+{
+    int k = n;
+    for (int j = 0; j < k ; j++)
+    {
+        int max = ptrarr[0], nom = 0;
+        for (int i = 0; i < k; i++)
+        {
+            if (ptrarr[i] > max) 
+            {
+                max = ptrarr[i];
+                nom = i;
+            }
+            int b = ptrarr[k - 1];
+            ptrarr[k - 1] = ptrarr[nom];
+            ptrarr[nom] = b;
+            k--;
         }
     }
 }
